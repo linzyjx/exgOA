f2c5462c9b9d833d9a455c9c3a8580cd515d530a
linzy <linzyjx@foxmail.com>
2019-04-08 12:31:44 +0800

    测试date_format_week函数

--- functions.php
+++ functions.php
@@ -5,26 +5,30 @@
  * Date: 2018/05/02
  * Time: 17:37
  */
-include_once ("inc.php");
+include_once("inc.php");
 
-function format_week(int $w){
-    $week_row=array(
-      '1'=>'星期一',
-      '2'=>'星期二',
-      '3'=>'星期三',
-      '4'=>'星期四',
-      '5'=>'星期五',
-      '6'=>'星期六',
-      '7'=>'星期天'
+function format_week(int $w)
+{
+    $week_row = array(
+        '1' => '星期一',
+        '2' => '星期二',
+        '3' => '星期三',
+        '4' => '星期四',
+        '5' => '星期五',
+        '6' => '星期六',
+        '7' => '星期天'
     );
-    return($week_row[$w]);
+    return ($week_row[$w]);
 }
 
-function date_format_week($t_date){
-    return(format_week(date("w",strtotime($t_date))));
+function date_format_week($t_date)
+{
+    echo "<!--" . $t_date . "-->";
+    return (format_week(date("w", strtotime($t_date))));
 }
 
-function getstatus($status, $position) {
+function getstatus($status, $position)
+{
     $t = $status & pow(2, $position - 1) ? 1 : 0;
     return $t;
 }
